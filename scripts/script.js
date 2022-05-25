a54dbbb6fb275cc2559bad1f94b0c80ef5b4a6ad
AnthonyGael <anthonygaelmayol@gmail.com>
2022-05-25 18:56:06 -0300

    Starting guess number game from mdn

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,27 @@
 document.addEventListener("DOMContentLoaded", function() {
-    function createParagraph() {
+  // cera un nuevo parrafo y lo añade al final del body  
+  function createParagraph() {
       let para = document.createElement('p');
       para.textContent = 'You clicked the button!';
       document.body.appendChild(para);
+      console.log(buttons.length);
     } 
+/*
+  1. Obtiene referencias de todos los botones de la página en un formato de arreglo.
+  2. Recorre todos los botones y agrega un detector de eventos 'click' a cada uno.
 
+  Cuando se presione cualquier botón, se ejecutará la función createParagraph().
+*/
     const buttons = document.querySelectorAll('button');
 
     for(let i=0; i < buttons.length; i++) {
       buttons[i].addEventListener('click', createParagraph);
     }
   });
+
+  // soy un comentario
+  /* nosotros
+  tambien
+  somos
+  un
+  comentario */
